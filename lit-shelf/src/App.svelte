39e450a846bc24222d1982982ff7d61bbbef118f
Clayton <ccrenshaw2012@gmail.com>
2025-04-04 17:56:55 -0500

    added login page

--- lit-shelf/src/App.svelte
+++ lit-shelf/src/App.svelte
@@ -1,47 +1,101 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import viteLogo from '/vite.svg'
-  import Counter from './lib/Counter.svelte'
+  
 </script>
 
 <main>
-  <div>
-    <a href="https://vite.dev" target="_blank" rel="noreferrer">
-      <img src={viteLogo} class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
+  <div class="w3-container">
+    <h1 style="text-align: center;">LitShelf</h1>
+    <section class="login-container">
+      <div class="left-half">
+        <img src="static/images/images/robot.png" alt="We love to read" class="placeholder-image"/>
+      </div>
+      <div class="right-half">
+        <div class="right-half-vertical-container" id="replaceableContainer">
+            <div class="login-header-container">
+              <h2>Login</h2>
+            </div>
+            <form class="login-form" id="login-form" method="post" action="/login">
+              <input type="text" placeholder="Username" name="username" class="input-field" required/>
+              <input type="password" placeholder="Password" name="password" class="input-field" required/>
+              <button class="login-button button-primary" type="submit" id="loginButton">Login</button>
+            </form>
+            <!--<div>
+              <button class="button-primary" 
+                style="margin-top: 20px;" 
+                id="registerButton"><a x-target="replaceableContainer"
+                method="get" href="/switch_login_form">Register</a>
+              </button>
+            </div>-->
+        </div>
+      </div>
+    </section>
   </div>
-  <h1>Vite + Svelte</h1>
-
-  <div class="card">
-    <Counter />
-  </div>
-
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
-  }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.svelte:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
-  }
-  .read-the-docs {
-    color: #888;
-  }
+
+.login-container {
+	display: flex;
+	margin: 50px;
+}
+
+.left-half {
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	border-right: 1px solid rgba(0, 0, 0, 0.1);
+}
+
+.placeholder-image {
+	width: 50%;
+	height: auto;
+	border-radius: 50%;
+}
+
+.right-half {
+	flex: 1;
+	display: flex;
+	justify-content: center;
+	align-items: center;
+}
+
+.login-form {
+	display: flex;
+	flex-direction: column;
+	gap: 20px;
+	width: 250px;
+}
+
+.input-field {
+	padding: 10px;
+	border: 1px solid #ccc;
+	border-radius: 5px;
+}
+
+.login-button {
+	padding: 10px;
+	background-color: #007bff;
+	color: white;
+	border: none;
+	border-radius: 5px;
+	cursor: pointer;
+}
+
+.login-button:hover {
+	background-color: #0056b3;
+}  
+
+.login-header-container {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	font-size: 1rem;
+}
+
+.right-half-vertical-container {
+	display: flex;
+	flex-direction: column;
+}
+  
 </style>
