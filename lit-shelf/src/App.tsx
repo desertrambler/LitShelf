a9decf35776211aaff764eb9ef32c5cf77733060
Clayton <ccrenshaw2012@gmail.com>
2025-04-04 20:09:20 -0500

    converted to bootstrap

--- lit-shelf/src/App.tsx
+++ lit-shelf/src/App.tsx
@@ -1,41 +1,42 @@
 import robot from './assets/images/botty.png';
 import type { Component } from 'solid-js';
-import 'bulma/css/bulma.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 const App: Component = () => {
   return (
-    <div class="appContainer">
-      <nav class="navbar" role="navigation" aria-label="main navigation">
-        <div id="navbarBasicExample" class="navbar-menu">
-          <div class="navbar-start">
-            <a class="navbar-item">
-              Home / Dashboard
-            </a>
-
-            <a class="navbar-item">
-              My Library
-            </a>
-
-            <a class="navbar-item">
-              Book Clubs
-            </a>
-          </div>
-          <div class="navbar-end">
-            <a class="navbar-item">
-              Settings / Profile
-            </a>
-          </div>
+    <div class="app-container">
+      <nav class="navbar navbar-expand-lg">
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link" href="#">Home / Dashboard</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">My Library</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Book Clubs</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">Settings / Profile</a>
+            </li>
+          </ul>
         </div>
       </nav>
+
       <div class="container">
-        <h1 style="text-align: center;">LitShelf</h1>
-        <section class="login-container">
-          <div class="left-half">
-            <img src={robot} alt="We love to read" class="placeholder-image"/>
+        <h1 class="text-center">LitShelf</h1>
+        <div class="d-flex justify-content-around align-items-center">
+          <div class="d-flex justify-content-center align-items-center">
+            <img src={robot} alt="We love to read" class="login-image"/>
           </div>
-          <div class="right-half">
-            <div class="right-half-vertical-container" id="replaceableContainer">
-                <div class="login-header-container">
+          <div class="d-flex justify-content-center align-items-center">
+            <div class="d-flex flex-column">
+                <div class="login-header-container d-flex justify-content-center align-items-center">
                   <h2>Login</h2>
                 </div>
                 <form class="login-form" id="login-form" method="post" action="/login">
@@ -51,7 +52,7 @@ const App: Component = () => {
                 </div>
             </div>
           </div>
-        </section>
+        </div>
       </div>
     </div>
   );
